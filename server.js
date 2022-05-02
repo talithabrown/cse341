b965ca7bfd91f571e53ab42d8bf7e55b3c256b3c
Talitha Brown <talithabrown128@gmail.com>
2022-05-02 15:44:29 -0700

    connected to mongodb

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
+
+
+//ugly code
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(process.env.MONGODB_URI, function(err, db){
+    if (err) throw err;
+    var dbo = db.db("contacts");
+    dbo.collection("contacts").find().toArray(function(err, result){
+        if (err) throw err;
+        console.log(result);
+        db.close()
+    })
+ })
