0fba32d27d502ec773c7a3098839e0d94ad3fab2
Talitha Brown <talithabrown128@gmail.com>
2022-05-02 16:23:18 -0700

    added contacts route

---
+++ routes/contacts.js
@@ -0,0 +1,22 @@
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+    //ugly code
+
+    const dotenv = require('dotenv');
+    dotenv.config();
+
+    const MongoClient = require('mongodb').MongoClient;
+    MongoClient.connect(process.env.MONGODB_URI, function(err, db){
+        if (err) throw err;
+        var dbo = db.db("contacts");
+        dbo.collection("contacts").find().toArray(function(err, result){
+            if (err) throw err;
+            res.json(result);
+            db.close()
+        })
+    })
+
+});
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -2,27 +2,8 @@ const express = require('express');
 const app =  express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Brayan Garcia')
-})
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
-
-
-//ugly code
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(process.env.MONGODB_URI, function(err, db){
-    if (err) throw err;
-    var dbo = db.db("contacts");
-    dbo.collection("contacts").find().toArray(function(err, result){
-        if (err) throw err;
-        console.log(result);
-        db.close()
-    })
- })
