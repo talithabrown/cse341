92e0520b5376059fa843b9a8fdc4c1fba0aeeb41
Talitha Brown <talithabrown128@gmail.com>
2022-05-02 17:28:46 -0700

    add rest

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,28 @@
 const routes = require('express').Router();
-const connect = require('../db/connect')
+const connect = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId
 
 routes.get('/', (req, res) => {   
-    connect.getCollection().find().toArray(function(err, result){
-        if (err) throw err;
-        res.json(result);
-        console.log('we got the contacts query successfully')
+
+    const results = connect.getCollection().find();
+    
+    results.toArray().then((documents) => {
+        res.status(200).json(documents);
+        console.log('returned all contacts')
     });
+
+});
+
+routes.get('/:id', (req, res) => {   
+
+    const contactId = new ObjectId(req.params.id);
+    const results = connect.getCollection().find({_id: contactId});
+    
+    results.toArray().then((documents) => {
+        res.status(200).json(documents[0]);
+        console.log(`returned one contact with id ${req.params.id}`)
+    });
+
 });
 
 module.exports = routes;
