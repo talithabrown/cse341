447ef45d736072450bdbe5069e7af292cd7c9191
Talitha Brown <talithabrown128@gmail.com>
2022-05-09 15:47:40 -0700

    week 4  I added documentation using swagger

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 
 routes.use('/', require('./home'));
 routes.use('/contacts', require('./contacts'));
+routes.use('/', require('./swagger'));
 
 module.exports = routes;
