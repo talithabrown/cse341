b8e58ee792a40f34dd9ce73fceac8b3f8bc21e81
Talitha Brown <talithabrown128@gmail.com>
2022-05-12 13:15:26 -0700

    changed server.js back to what it was before

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-//const res = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 3000;
 const connect = require('./db/connect');
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', ['PUT', 'GET', 'POST', 'DELETE', 'OPTIONS']);
-    next();
-  })
-  .use('/', require('./routes'));
-
 connect.initDatabase();
+
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-//"cse341-backend2.herokuapp.com",
+//"cse341-backend2.herokuapp.com"
