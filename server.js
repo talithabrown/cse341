2b2bf097f797541c19ce8f8eb5b772101547b70b
Talitha Brown <talithabrown128@gmail.com>
2022-05-12 11:37:26 -0700

    made changes to allow front end to use api

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+//const res = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 3000;
 const connect = require('./db/connect');
 
-connect.initDatabase();
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', ['PUT', 'GET', 'POST', 'DELETE', 'OPTIONS']);
+    next();
+  })
+  .use('/', require('./routes'));
 
-app.use('/', require('./routes'));
+connect.initDatabase();
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
